3cfa3485a514ccbe450458d1751536e253ca27f0
unknown <shreyagarwal611@gmail.com>
2020-06-05 01:30:35 +0530

    remove mail serivce

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,7 +15,7 @@ router.post('/users', async (req,res)=>{
     
     try{
     await user.save()
-    await sendWelcomeEmail(user.email,user.name)
+    // await sendWelcomeEmail(user.email,user.name)
     const token = await user.generateAuthToken()
     res.status(201).send({user,token})
     }catch(e){
